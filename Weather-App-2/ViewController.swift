c6e5a6bb70444b3509ea665396431c8576542e15
Mitchell Hudson <soggybag@gmail.com>
2015-11-12 09:13:34 -0800

    Initial Commit

---
+++ Weather-App-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather-App-2
+//
+//  Created by mitchell hudson on 11/12/15.
+//  Copyright © 2015 mitchell hudson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
